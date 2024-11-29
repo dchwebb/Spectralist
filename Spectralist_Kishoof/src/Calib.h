b2c3d7638e102b1afa941782e02eb4d5a37f12e9
Dominic Webb <dwebb@reapit.com>
2024-11-29 17:55:48 +0000

    Added calibration defaults

--- Spectralist_Kishoof/src/Calib.h
+++ Spectralist_Kishoof/src/Calib.h
@@ -8,7 +8,7 @@ public:
 	Calib();
 	void Calibrate(char key = 0);
 	static void UpdatePitchLUT();
-//	static void UpdateConfig();
+	static void UpdateConfig();
 
 	// 0v = 61200; 1v = 50110; 2v = 39020; 3v = 27910; 4v = 16790; 5v = 5670
 	// C: 16.35 Hz 32.70 Hz; 65.41 Hz; 130.81 Hz; 261.63 Hz; 523.25 Hz; 1046.50 Hz; 2093.00 Hz; 4186.01 Hz
@@ -19,16 +19,20 @@ public:
 
 	// Calculations: 11330 is difference in cv between two octaves; 50050 is cv at 1v and 130.81 is desired Hz at 1v
 
+	static constexpr float pitchBaseDef = (65.41f * (2048.0f / sampleRate)) / std::pow(2.0, -50050.0f / 11330.0f);
+	static constexpr float pitchMultDef = -1.0f / 11330.0f;
+	static constexpr uint16_t vcaNormalDef = 24000;
+
 	struct {
-		float pitchBase = (65.41f * (2048.0f / sampleRate)) / std::pow(2.0f, -50050.0f / 11330.0f);
-		float pitchMult = -1.0f / 11330.0f;
-		uint16_t vcaNormal = 24000;
+		float pitchBase = pitchBaseDef;
+		float pitchMult = pitchMultDef;
+		uint16_t vcaNormal = vcaNormalDef;
 	} cfg;
 
 	ConfigSaver configSaver = {
 		.settingsAddress = &cfg,
 		.settingsSize = sizeof(cfg),
-		.validateSettings = nullptr
+		.validateSettings = UpdateConfig
 	};
 
 	float pitchLUT[adcMax + 1];
